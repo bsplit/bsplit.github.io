aeb77e8932e8a493dbc58065005d7248b77fd2c5
bsplit <github@bensplit.com>
2016-07-29 23:17:32 -0400

    Absolute references. Minor tweaks.

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,6 @@ $(document).ready(function(){
 
 });
 
-	var sc_project=10987063; var sc_invisible=1; var sc_security="69b8be84"; 
-	var scJsHost = (("https:" == document.location.protocol) ?	"https://secure." : "http://www.");
-	document.write("<sc"+"ript type='text/javascript' src='" +	scJsHost+	"statcounter.com/counter/counter.js'></"+"script>");
+var sc_project=10987063; var sc_invisible=1; var sc_security="69b8be84"; 
+var scJsHost = (("https:" == document.location.protocol) ?	"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +	scJsHost+	"statcounter.com/counter/counter.js'></"+"script>");
